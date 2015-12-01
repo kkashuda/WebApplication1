bfa25c346d29e756f00dd6ef5c9f271aaf859461
llamallove12 <avhedges@crimson.ua.edu>
2015-12-01 15:27:48 -0600

    added jobs class

---
+++ WebApplication1/Models/Job.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace WebApplication1.Models
+{
+    public class Job
+    {
+        public string employer { get; set; }
+        public string title { get; set; }
+        public string description { get; set;}
+        public Boolean interested { get; set; }
+    }
+    public class JobDBContext : DbContext
+    {
+        public DbSet<Job> Jobs { get; set;}
+    }
+}
