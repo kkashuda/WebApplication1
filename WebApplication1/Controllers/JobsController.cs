2c799a76efe91b78923cdd46cd7e57403e9e2a2d
llamallove12 <avhedges@crimson.ua.edu>
2015-12-03 14:33:37 -0600

    added email to controller

--- WebApplication1/Controllers/JobsController.cs
+++ WebApplication1/Controllers/JobsController.cs
@@ -46,7 +46,7 @@ namespace WebApplication1.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Employer,Title,Description,Interested")] Job job)
+        public ActionResult Create([Bind(Include = "Employer,Title,Description,Interested,Email")] Job job)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace WebApplication1.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Employer,Title,Description,Interested")] Job job)
+        public ActionResult Edit([Bind(Include = "Employer,Title,Description,Interested,Email")] Job job)
         {
             if (ModelState.IsValid)
             {
